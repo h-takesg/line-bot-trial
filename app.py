c101c9705a67a1b2e74e38f1d3a15ef8b04d8408
h-takesg <82633007+h-takesg@users.noreply.github.com>
2024-04-14 05:41:04 +0000

    おうむがえしできる

---
+++ app.py
@@ -0,0 +1,79 @@
+from flask import Flask, request, abort
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging import (
+	ApiClient, Configuration, MessagingApi,
+	ReplyMessageRequest, PushMessageRequest,
+	TextMessage, PostbackAction
+)
+from linebot.v3.webhooks import (
+	FollowEvent, MessageEvent, PostbackEvent, TextMessageContent
+)
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
+CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
+
+## Flask アプリのインスタンス化
+app = Flask(__name__)
+
+## LINE のアクセストークン読み込み
+configuration = Configuration(access_token=CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
+
+## コールバックのおまじない
+@app.route("/callback", methods=['POST'])
+def callback():
+	# get X-Line-Signature header value
+	signature = request.headers['X-Line-Signature']
+
+	# get request body as text
+	body = request.get_data(as_text=True)
+	app.logger.info("Request body: " + body)
+
+	# handle webhook body
+	try:
+		handler.handle(body, signature)
+	except InvalidSignatureError:
+		app.logger.error("Invalid signature. Please check your channel access token/channel secret.")
+		abort(400)
+
+	return 'OK'
+
+@handler.add(FollowEvent)
+def handle_follow(event):
+	## APIインスタンス化
+	with ApiClient(configuration) as api_client:
+		line_bot_api = MessagingApi(api_client)
+
+	## 返信
+	line_bot_api.reply_message(ReplyMessageRequest(
+		replyToken=event.reply_token,
+		messages=[TextMessage(text="Thank You!")]
+	))
+
+## オウム返しメッセージ
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+	## APIインスタンス化
+	with ApiClient(configuration) as api_client:
+		line_bot_api = MessagingApi(api_client)
+
+	## 受信メッセージの中身を取得
+	received_message = event.message.text
+
+	## APIを呼んで送信者のプロフィール取得
+	profile = line_bot_api.get_profile(event.source.user_id)
+	display_name = profile.display_name
+
+	## 返信メッセージ編集
+	reply = f'{display_name}さんのメッセージ\n{received_message}'
+
+	## オウム返し
+	line_bot_api.reply_message(ReplyMessageRequest(
+		replyToken=event.reply_token,
+		messages=[TextMessage(text=reply)]
+	))
---
+++ readme.md
@@ -0,0 +1,7 @@
+## pip
+書き出し：`pip freeze > requirements.txt`
+読み込み：`pip install -r requirements.txt`
+
+## run
+`flask run --debug --reload`
+ポート5000で走る
